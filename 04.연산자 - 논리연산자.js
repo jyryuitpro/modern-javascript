5e2506682cd53bf1581dda8ca825472326a8420a
jyryuitpro <jy.ryu.itpro@gmail.com>
2021-01-04 11:44:05 +0900

    04. 연산자 - 논리연산자

---
+++ 04.연산자 - 논리연산자.js
@@ -0,0 +1,31 @@
+// let value = true;
+// value = false;
+
+// 논리연산자 순서 : NOT > AND > OR
+// NOT !
+// AND &&
+// OR ||
+
+// const a = !true;
+// const a = !false;
+// console.log(a);
+
+// const a = true && true;
+// const a = false && true;
+// const a = true && false;
+// const a = false && false;
+
+// const a = true || true;
+// const a = true || false;
+// const a = false || true;
+// const a = false || false;
+
+const value = !(true && false || true && false || !false);
+// !(true && false || true && false || true)
+// !(false || true && false || true)
+// !(false || false || true)
+// !(false || true)
+// !(true)
+// false
+
+console.log(a);
