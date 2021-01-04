021bc9ad56ea19ff37d061b3fa29e6c890f57026
jyryuitpro <jy.ryu.itpro@gmail.com>
2021-01-04 11:29:51 +0900

    02. 변수

---
+++ 02.변수.js
@@ -0,0 +1,35 @@
+console.log('Hello!');
+
+let value = 1;
+// SyntaxError: Identifier 'value' has already been declared
+// let value = 2;
+
+console.log(value);
+
+value = 2;
+
+console.log(value);
+
+const a = 1;
+// SyntaxError: Identifier 'a' has already been declared
+// const a = 1;
+
+// TypeError: Assignment to constant variable.
+// a = 2;
+
+// var a = 1;
+// var a = 2;
+
+let text = 'hello';
+let name = "헬로우 자바스크립트";
+
+// let good = true;
+let good = null;
+let loading = false;
+let something = undefined;
+
+let friend = null;
+
+// undefined
+let criminal;
+console.log(criminal);
