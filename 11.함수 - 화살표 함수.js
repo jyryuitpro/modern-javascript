a5fe9e500e402e42ecf73e42a3b4144cce49ecea
jyryuitpro <jy.ryu.itpro@gmail.com>
2021-01-04 14:15:27 +0900

    11. 함수 - 화살표 함수

---
+++ 11.함수 - 화살표 함수.js
@@ -0,0 +1,14 @@
+// const add = (a, b) => {
+//     return a + b;
+// };
+
+const add = (a, b) => a + b;
+
+const sum = add(1, 2);
+console.log(sum);
+
+const hello = (name) => {
+    console.log(`Hello, ${name}!`);
+};
+
+hello('Ryu');
