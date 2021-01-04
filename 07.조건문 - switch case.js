21f2e80067d5b7d8f9bb4f739e6728f0851832b0
jyryuitpro <jy.ryu.itpro@gmail.com>
2021-01-04 13:25:08 +0900

    07. 조건문 - switch case

---
+++ 07.조건문 - switch case.js
@@ -0,0 +1,18 @@
+const device = "iphone";
+// const device = "ipad";
+// const device = "galaxy note";
+// const device = "macbook";
+
+switch (device) {
+    case "iphone":
+        console.log("아이폰!");
+        break;
+    case "ipad":
+        console.log("아이패드!");
+        break;
+    case "galaxy note":
+        console.log("갤럭시 노트!");
+        break;
+    default:
+        console.log("모르겠네요..");
+}
