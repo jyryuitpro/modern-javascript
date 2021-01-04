b5cc4e21dad2e2b81240ab3b11312da0feb08cad
jyryuitpro <jy.ryu.itpro@gmail.com>
2021-01-04 11:34:59 +0900

    03. 연산자 - 산술연산자, 대입연산자

---
+++ 03.연산자 - 산술연산자, 대입연산자.js
@@ -0,0 +1,18 @@
+let value = 1;
+
+// let a = 1 + 2;
+// let a = 1 + 2 - (3 * 4) / 4;
+
+let a = 1;
+// console.log(a++);
+// console.log(a);
+// console.log(++a);
+
+// console.log(a--);
+// console.log(a);
+
+a += 3;
+a -= 3;
+a *= 3;
+a /= 3;
+console.log(a);
