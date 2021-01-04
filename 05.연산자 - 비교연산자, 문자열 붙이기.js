25075db9a661aec57dc1edc3757c35048fa67c05
jyryuitpro <jy.ryu.itpro@gmail.com>
2021-01-04 11:52:23 +0900

    05. 연산자 - 비교연산자, 문자열 붙이기

---
+++ 05.연산자 - 비교연산자, 문자열 붙이기.js
@@ -0,0 +1,26 @@
+// const a = 'a';
+// const b = 'b';
+
+// const equals = a === b;
+// const notEquals = a !== b;
+
+// 타입을 검사하지 않는다
+// const equals = a == b;
+// const equals = a != b;
+
+// console.log(notEquals);
+
+// const a = 10;
+// const b = 15;
+// const c = 15;
+
+// console.log(a < b);
+// console.log(b > a);
+// console.log(b >= a);
+// console.log(a <= c);
+// console.log(b < c);
+
+const a = "안녕";
+const b = "하세요";
+
+console.log(a + b);
