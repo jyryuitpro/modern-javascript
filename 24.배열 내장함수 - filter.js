4ce01c5da24db48c25c152d23e85eef8a5bc6387
jyryuitpro <jy.ryu.itpro@gmail.com>
2021-01-10 13:58:21 +0900

    24. 배열 내장함수 - filter

---
+++ 24.배열 내장함수 - filter.js
@@ -0,0 +1,28 @@
+const todos = [
+    {
+        id: 1,
+        text: '자바스크립트 입문',
+        done: true,
+    },
+    {
+        id: 2,
+        text: '함수 배우기',
+        done: true,
+    },
+    {
+        id: 3,
+        text: '객체와 배열 배우기',
+        done: true,
+    },
+    {
+        id: 4,
+        text: '배열 내장함수 배우기',
+        done: false,
+    }
+];
+
+const tasksNotDone = todos.filter(todo => todo.done === false);
+// const tasksNotDone = todos.filter(todo => !todo.done); // false
+// const tasksNotDone = todos.filter(todo => !todo.done); // true
+// const tasksNotDone = todos.filter(todo => todo.done === true);
+console.log(tasksNotDone);
