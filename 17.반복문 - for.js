1f36b1b8ef76f6767c9e4d8538a28e2e28066848
jyryuitpro <jy.ryu.itpro@gmail.com>
2021-01-10 10:12:33 +0900

    17. 반복문 - for

---
+++ 17.반복문 - for.js
@@ -0,0 +1,17 @@
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+}
+
+for (let i = 10; i > 0; i--) {
+    console.log(i);
+}
+
+for (let i = 10; i > 0; i-=2) {
+    console.log(i);
+}
+
+const names = ['멍멍이', '야옹이', '멍뭉이'];
+
+for (let i = 0; i < names.length; i++) {
+    console.log(names[i]);
+}
